386d726b1f1b3f6264940db8694bf79d18e041da
Matt Wade <matteowade@gmail.com>
2019-06-01 14:55:15 -0600

    add sales data chart

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <sales></sales>
+  </div>
+</template>
+
+<script>
+import Sales from './Sales.vue'
+
+export default {
+  name: 'Dashboard',
+  components: {
+    sales: Sales
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Sales.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="hello">
+    <apexchart v-if="series.length" type="bar" width="800" :options="chartOptions" :series="series"></apexchart>
+  </div>
+</template>
+
+<script>
+import VueApexCharts from 'vue-apexcharts'
+import SalesData from '../sales-data.json'
+
+const moneyFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 1
+})
+
+export default {
+  name: 'Sales',
+  components: {
+    'apexchart': VueApexCharts
+  },
+  data: function() {
+    return {
+      salesData: SalesData,
+      series: [],
+      chartOptions: {
+        plotOptions: {
+          bar: {
+            horizontal: true,
+          }
+        },
+        xaxis: {
+          categories: [],
+          labels: {
+            formatter: function (val) {
+              return moneyFormatter.format(val)
+            }
+          }
+        },
+        dataLabels: {
+          enabled: true,
+          formatter: function(val) {
+            return moneyFormatter.format(val)
+          },
+          offsetX: 0,
+        }
+      }
+    }
+  },
+  methods: {
+    region: function() {
+      return Object.keys(this.salesData)
+    },
+    profitTotals: function() {
+      let values = []
+
+      Object.entries(this.salesData).forEach(([key, value]) => {
+        let profitSum = 0
+        for (let i = 0; i < value.length; i++) {
+          profitSum += value[i]['total-profit']
+        }
+        values.push(Number(profitSum))
+      })
+      return values
+    }
+  },
+  mounted: function() {
+    this.series = [{data: this.profitTotals()}]
+    this.chartOptions.xaxis.categories = this.region()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
